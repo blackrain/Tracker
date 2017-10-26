7205e20d2db5cb5132997aa674632df179b61d68
Michał Januszewski <michal.januszewski@10clouds.com>
2017-10-26 16:22:02 +0200

    Initial Commit

---
+++ MovesClone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovesClone
+//
+//  Created by Michał Januszewski on 26/10/2017.
+//  Copyright © 2017 GearAppa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovesClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovesClone
+//
+//  Created by Michał Januszewski on 26/10/2017.
+//  Copyright © 2017 GearAppa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
